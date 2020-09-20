7994c3a57c8a134b442c66df37aebdcc94e1e0ab
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-20 15:48:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,7 +42,7 @@ def kesha_maker(array)
   counter = 0
   new_array = []
   while counter < array.length 
-    new_array << array[item][0..1] + "$" + array[item][3..-1]
+    new_array << array[counter][0..1] + "$" + array[counter][3..-1]
     counter += 1
   end
   #puts array[0][0..1] + "$" + array[0][3..-1]
