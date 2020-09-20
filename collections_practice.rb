77d7992acd6cfd257c439def485c670f98873bac
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-20 15:45:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ end
 
 def kesha_maker(array)
   array.collect {|item|
-    array[0][0..1] + "$" + array[0][3..-1]
+    array[item][0..1] + "$" + array[item][3..-1]
   }
   #puts array[0][0..1] + "$" + array[0][3..-1]
 end
